1225b9cd4bc732f3d5b5658a878e57727eaeee7e
Nannapat2546 <655021000089@mail.rmutk.ac.th>
2026-09-19 22:54:20 +0700

    Update login.js

--- screens/login.js
+++ screens/login.js
@@ -22,7 +22,7 @@ export default function Login({ navigation }) {
     }
 
     try {
-      const response = await fetch('https://app-rmutk-sports.onrender.com/api/login', {
+      const response = await fetch('https://rmutk-sport.onrender.com/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
