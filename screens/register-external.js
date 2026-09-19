80e13cd41d72cc00dd654cd7a49fdcfce76185cf
Nannapat2546 <655021000089@mail.rmutk.ac.th>
2026-09-20 01:02:37 +0700

    Update register-external.js

--- screens/register-external.js
+++ screens/register-external.js
@@ -94,7 +94,7 @@ export default function RegisterOutsider({ navigation }) {
   // 🌟 ฟังก์ชันจัดการข้อมูล OCR (ส่งรูปไป Backend และดึงข้อความกลับมา)
   const processOcrData = async (base64Image) => {
     try {
-      const response = await fetch('https://app-rmutk-sports.onrender.com/api/ocr', {
+      const response = await fetch('https://rmutk-sport.onrender.com/api/ocr', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ image: base64Image })
@@ -208,7 +208,7 @@ export default function RegisterOutsider({ navigation }) {
     };
 
     try {
-      const response = await fetch('https://app-rmutk-sports.onrender.com/api/register/outsider', {
+      const response = await fetch('https://rmutk-sport.onrender.com/api/register/outsider', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(outsiderData),
