2ad2baff7b86a6c88970d551636ce05f39667f47
Nannapat2546 <655021000089@mail.rmutk.ac.th>
2026-09-19 21:20:02 +0700

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
+// ===========================================================================
+// 🌟 เพิ่ม Route สำหรับหน้าแรก (แก้ Error Cannot GET /)
+// ===========================================================================
+app.get('/', (req, res) => {
+  res.send('RMUTK Sport API is running!');
+});
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL, // ใช้สำหรับเชื่อมต่อบน Render อัตโนมัติ
   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false, // จำเป็นสำหรับ PostgreSQL บนคลาวด์
