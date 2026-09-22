10a8688b5c95f63aa14a5b1a63b8fe5f2bccfcb6
Nannapat2546 <655021000089@mail.rmutk.ac.th>
2026-09-22 13:51:04 +0700

    Update loginadmin.js

--- screens/loginadmin.js
+++ screens/loginadmin.js
@@ -53,7 +53,7 @@ export default function LoginScreen({ navigation }) {
     setLoading(true);
 
     try {
-      const response = await fetch('https://rmutk-sport.onrender.com/api/login-admin', {
+      const response = await fetch('https://envision-stumble-kept.ngrok-free.dev/api/login-admin', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
