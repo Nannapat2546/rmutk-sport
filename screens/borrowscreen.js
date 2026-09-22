216034e679504e40f89faf8b9b75b31ccc246f69
Nannapat2546 <655021000089@mail.rmutk.ac.th>
2026-09-22 13:48:32 +0700

    Update borrowscreen.js

--- screens/borrowscreen.js
+++ screens/borrowscreen.js
@@ -52,7 +52,7 @@ const CustomDropdown = ({ label, options, selectedValue, onSelect, placeholder }
 
 export default function BorrowScreen({ navigation, route }) {
   const qrData = route.params?.qrData || '';
-  const API_URL = 'https://rmutk-sport.onrender.com'; 
+  const API_URL = 'https://envision-stumble-kept.ngrok-free.dev'; 
   
   const [isLoading, setIsLoading] = useState(true);
   const [userData, setUserData] = useState(null);
